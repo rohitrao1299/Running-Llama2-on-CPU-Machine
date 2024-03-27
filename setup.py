e83136da5b0adff5309fe4b7180f2e57d81d0420
Rohit Rao <rohitk121999@gmail.com>
2024-03-27 20:58:46 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rohit Kumar',
+    author_email= 'rohitk121999@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
